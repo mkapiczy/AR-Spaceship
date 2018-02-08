acda7dd43af4d2e5f28dd392f8a239d027557424
Michal Kapiczynski <michal.kapiczynski@sirenia.eu>
2018-02-08 11:54:30 +0100

    Initial project setup - Scenes, Vuforia

---
+++ Assets/Scripts/MoonRotator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoonRotator : MonoBehaviour {
+
+	public float spinSpeed;
+	public float orbitSpeed;
+	private GameObject earthObject;
+
+
+	// Use this for initialization
+	void Start () {
+		earthObject = GameObject.Find("Earth");
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.RotateAround (earthObject.transform.position, Vector3.up, orbitSpeed * Time.deltaTime);
+	}
+}
