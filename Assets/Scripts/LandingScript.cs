80cad47c9128e37f14001c310010c336e8f5aa4e
Michal Kapiczynski <michal.kapiczynski@sirenia.eu>
2018-02-09 17:00:33 +0100

    Landing Script rename

--- Assets/Scripts/LandingScript.cs
+++ Assets/Scripts/LandingScript.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Script : MonoBehaviour {
+public class LandingScript : MonoBehaviour {
 
 	private GameObject airway;
 	private GameObject plane;
