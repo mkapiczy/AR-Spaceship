f014de6d6015e0239574d02993c11c6198bfc459
Ghaith Ali <ghaith.ali@meet2talk.com>
2018-02-09 16:47:03 +0100

    started working on scene e

---
+++ Assets/Scripts/FalconScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FalconScript : MonoBehaviour {
+	private GameObject falcon;
+	private GameObject enemyShip;
+
+	// Use this for initialization
+	void Start () {
+		falcon = GameObject.Find("falcon");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 falconForward = falcon.transform.forward;
+
+		Debug.Log ("test");
+	}
+}
