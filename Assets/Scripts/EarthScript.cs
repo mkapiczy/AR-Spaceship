cc7fbbb6f495ec39be46eb594f60a343e303b078
Michal Kapiczynski <michal.kapiczynski@sirenia.eu>
2018-02-10 15:41:21 +0100

    d) task finished. Properly translate between plane's nose, and earth coordinate system, and detects nose's position over the earth

--- Assets/Scripts/EarthScript.cs
+++ Assets/Scripts/EarthScript.cs
@@ -7,15 +7,13 @@ public class EarthScript : MonoBehaviour {
 	private GameObject earth;
 	private GameObject plane;
 	private GameObject planeNose;
+
 	private Vector3 nosePositionInRelationToEarth;
-	private Vector3 positionTest;
+	private string label;
 
+	private float earthRadius = 0.38f;
+	private float yBoundary = 0.7f;
 
-	private float xBoundary = 0.5f;
-	private float yBoundary = 0.5f;
-	private float zBoundary = 0.5f;
-
-	// Use this for initialization
 	void Start () {
 		earth = GameObject.Find ("Earth");
 		plane = GameObject.Find ("Plane");
@@ -24,22 +22,35 @@ public class EarthScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 nosePosition = planeNose.transform.localPosition;
-
+		// Once per frame
 		Matrix4x4 earthTransform = earth.transform.localToWorldMatrix;
 		Matrix4x4 planeTransform = plane.transform.localToWorldMatrix;
-
 		Matrix4x4 transformToEarthLocalCoordinates = earthTransform.inverse * planeTransform;
 
-		nosePositionInRelationToEarth = transformToEarthLocalCoordinates * nosePosition;
-		positionTest = earth.transform.position;
+		// Once per point
+		Vector3 localNosePosition = planeNose.transform.localPosition;
+		nosePositionInRelationToEarth = transformToEarthLocalCoordinates.MultiplyPoint3x4(localNosePosition);
 
+		if (IsInsideEarth()) {
+			if (nosePositionInRelationToEarth.z > 0) {
+				label = "Hemisphere: North";
+			} else {
+				label = "Hemisphere: South";
+			}
+		} else {
+			label = "" + Vector3.Distance (new Vector3 (nosePositionInRelationToEarth.x, 0, nosePositionInRelationToEarth.z), new Vector3 (0, 0, 0));
+		}
 	}
 
 	private void OnGUI(){
 		GUI.color = Color.red;
 		GUI.Label (new Rect (10, 10, 500, 100), "Local position: " + nosePositionInRelationToEarth.x + " " + nosePositionInRelationToEarth.y + " " + nosePositionInRelationToEarth.z);
-		GUI.Label (new Rect (10, 20, 500, 100), "Test position: " + positionTest.x + " " + positionTest.y + " " + positionTest.z);
-
+		GUI.Label (new Rect (10, 20, 500, 100), label);
 	}
+
+	private bool IsInsideEarth(){
+		return Vector3.Distance(new Vector3(nosePositionInRelationToEarth.x, 0, nosePositionInRelationToEarth.z), new Vector3(0,0,0)) <= earthRadius 
+			&& nosePositionInRelationToEarth.y >= -yBoundary && nosePositionInRelationToEarth.y <= yBoundary;
+	}
+		
 }
