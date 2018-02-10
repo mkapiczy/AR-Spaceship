158a15361a61a0ad7411f54e86e47fb23c28ee73
Ghaith Ali <ghaith.ali@meet2talk.com>
2018-02-10 15:39:33 +0100

    first part of assignment E is solved.

--- Assets/Scripts/FalconScript.cs
+++ Assets/Scripts/FalconScript.cs
@@ -9,33 +9,46 @@ public class FalconScript : MonoBehaviour {
 
 	public float rayLength = 20;
 	private Ray ray;
-	private bool drawLine;
+	private bool drawLine,isCollided;
 	private string status;
+	public GameObject explosion;
+
 
 	// Use this for initialization
 	void Start () {
 		Debug.Log ("Start");
 		falcon = GameObject.Find("falcon");
-		ray = new Ray (falcon.transform.position, falcon.transform.forward);
+		explosion = GameObject.Find ("ExplosionPrefab");
+		enemyShip = GameObject.Find ("enemyShip");
+		drawLine = false;
+		isCollided = false;
+		//make laser in scene
+		//ray = new Ray (falcon.transform.position, falcon.transform.forward);
 
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-
+		//toggle laser
 		if (Input.GetKeyDown ("space")) {
 			if (drawLine) {
 				drawLine = false;
 				Debug.Log("Laser stopped!");
 				status = "Laser stopped!";
-			
+
 			} else {
 				drawLine = true;
 				Debug.Log("Laser started!");
 				status = "Laser started!";
 			}
+		}
+		RaycastHit hit;
+		isCollided = Physics.Raycast (new Ray (falcon.transform.position, falcon.transform.forward), out hit);
 
-			
+		if (isCollided && drawLine) {
+			GameObject explosionInstance = Instantiate (explosion,  hit.point, Quaternion.identity);
+			Destroy (explosionInstance, 10f);
+			Debug.Log ("HIT");
 		}
 	}
 
@@ -45,17 +58,21 @@ public class FalconScript : MonoBehaviour {
 	}
 
 	void OnRenderObject(){
+		//make laser on GUI
+		//TODO:: change this to drawLine instead of true.
 		if (drawLine) {
 			if (material == null) {
 				material = new Material (Shader.Find ("Hidden/Internal-Colored"));
 			}	
-			Debug.Log ("HERE");
 			material.SetPass (0);
 			GL.Begin (GL.LINES);
 			GL.Color (Color.red);
 			GL.Vertex (falcon.transform.position);
 			GL.Vertex (falcon.transform.position + falcon.transform.forward * rayLength);
 			GL.End ();
+
+		} else {
+			material = null;
 
 		}
 	}
